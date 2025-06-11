047a2af058b9c66670737444c49662827e1d4405
sakshammishraGla <sakshammishra5thsep@gmail.com>
2025-06-11 12:34:03 +0530

    Fix rendering of Education and Experience sections

---
+++ app/ClientHome.jsx
@@ -0,0 +1,35 @@
+"use client"
+import { personalData } from "@/utils/data/personal-data";
+import AboutSection from "./components/homepage/about";
+import Blog from "./components/homepage/blog";
+import ContactSection from "./components/homepage/contact";
+// import Education from "./components/homepage/education";
+// import Experience from "./components/homepage/experience";
+import HeroSection from "./components/homepage/hero-section";
+import Projects from "./components/homepage/projects";
+import Skills from "./components/homepage/skills";
+
+import dynamic from "next/dynamic";
+
+const Experience = dynamic(() => import("./components/homepage/experience"), {
+  ssr: false,
+});
+const Education = dynamic(() => import("./components/homepage/education"), {
+  ssr: false,
+});
+
+export default function ClientHome({ blogs }) {
+  return (
+    <div suppressHydrationWarning>
+      <HeroSection />
+      <AboutSection />
+      <Experience />
+      <Skills />
+      <Projects />
+      <Education />
+      <Blog blogs={blogs} />
+      <ContactSection />
+      {/* <ScrollToTop /> */}
+    </div>
+  );
+}
